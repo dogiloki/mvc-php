74c87057c691a15db42e06cba2ab6d0622f22564
Julio Villanueva <wolfri2002@gmail.com>
2022-11-10 03:00:27 -0600

    Error de librería que no existen

--- models/prueba.php
+++ models/prueba.php
@@ -3,12 +3,11 @@
 namespace models;
 
 use libs\DB;
-use util\Secure;
 
 class Prueba{
 
 	public function modelar(){
-		return Secure::encodePassword("HOla");
+		return "Souy un modelado de datos";
 	}
 
 }
