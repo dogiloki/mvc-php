ec2fe74e95c8c10bab02b1ae37e0928edd3ebd41
dogiloki <wolftri2002@gmail.com>
2023-05-16 17:53:43 -0600

    Cambio en seeder por defecto

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,12 +5,13 @@ namespace database\seeders;
 use libs\DB\Seeder;
 use libs\DB\DB;
 use libs\Middle\Secure;
+use models\User;
 
 class UserSeeder extends Seeder{
 
 	public function run(){
 		for($index=0; $index<10; $index++){
-			DB::table('user')->insert([
+			User::create([
 				'name'=>$this->faker->name,
 				'email'=>$this->faker->email,
 				'password'=>Secure::encodePassword('123')
