e24807b8fc6eea5a1353fccb6400f43e041a365b
dogiloki <wolftri2002@gmail.com>
2023-05-21 17:16:12 -0600

    Se removio llamado de generación de token csrf

--- app/Middlewares/VerifyCsrfToken.php
+++ app/Middlewares/VerifyCsrfToken.php
@@ -10,17 +10,14 @@ class VerifyCsrfToken extends Middleware{
      public function handle(Request $request, \Closure $next){
           $csrf_token=$request->input('_token')??$request->header('X-CSRF-TOKEN')??$request->cookie()->get('CSRF_TOKEN')??null;
           if($csrf_token==null){
-               Request::csrfToken();
                $request->session()->regenerate();
                return abort(401,"CSRF token not found");
           }
-          if($csrf_token==$request->session()->get('_token')){
-               return $next($request);
-          }else{
-               Request::csrfToken();
+          if($csrf_token!=$request->session()->get('_token')){
                $request->session()->regenerate();
                return abort(401,"CSRF token invalid");
           }
+          return $next($request);
 	}     
 
 }
