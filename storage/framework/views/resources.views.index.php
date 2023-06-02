5ec0f63a0c752a366a41759cd5d968f2ab588228
Julio Villanueva <73675624+dogiloki@users.noreply.github.com>
2023-06-02 00:11:22 -0600

    Delete storage directory

--- storage/framework/views/resources.views.index.php
+++
@@ -1,8 +0,0 @@
-<?php view('layouts.header',["title"=>"Inicio"]); ?>
-<h1>Hola mundo</h1>
-<p>Bienvenido</p>
-<?php
-    $user=\app\Models\User::all();
-?>
-<component:vista :variable="<?php echo base64_encode(serialize($user)); ?>"/>
-<?php view('layouts.footer'); ?>
--- storage/framework/views/resources.views.layouts.footer.php
+++
@@ -1,7 +0,0 @@
-</body>
-</html>
-
-<script src="<?php echo url('js/fetch.js'); ?>"></script>
-<script src="<?php echo url('js/component/component.js'); ?>"></script>
-<script src="<?php echo url('js/component/wire.js'); ?>"></script>
-<script src="<?php echo url('js/component/app.js'); ?>"></script>
