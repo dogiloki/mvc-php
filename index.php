4194786018424fb5017639d7e369582309fa7e00
dogiloki <wolftri2002@gmail.com>
2023-10-20 12:22:03 -0600

    fix: debug code remover

--- index.php
+++ index.php
@@ -49,7 +49,6 @@ if(config()->app('debug')){
 }
 set_exception_handler("exception");
 set_error_handler("error");
-echo $hola;
 //$env->set('APP_URL',str_replace("\\","/",(isset($_SERVER['HTTPS'])?"https":"http")."://".$_SERVER["HTTP_HOST"]).\dirname($_SERVER['PHP_SELF'])."/");
 // Verificar archivos publicos
 $uri=$_SERVER['REQUEST_URI'];
