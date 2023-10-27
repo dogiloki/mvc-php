1e73f77e28f409032564dbe235d58b0aa3a7c1ae
dogiloki <wolftri2002@gmail.com>
2023-10-26 22:36:28 -0600

    style: space

--- app/Services/MiddlewareService.php
+++ app/Services/MiddlewareService.php
@@ -11,18 +11,18 @@ class MiddlewareService implements ServiceImpl{
 		// Middleware globales
 		Middleware::middleware([
 			\libs\Session\Middleware\StartSession::class,
-            \app\Middlewares\VerifyCsrfToken::class
+			\app\Middlewares\VerifyCsrfToken::class
 		]);
 		// Middleware archivos en específico
 
 		// Middleware alias
 		Middleware::middlewareAlias([
 			'auth'=>\app\Middlewares\Authenticate::class,
-	        'auth_api'=>\app\Middlewares\AuthenticateApi::class,
-	        'guest'=>\app\Middlewares\Guest::class,
-	        'verify_email'=>\app\Middlewares\VerifyEmail::class,
-	        'csrf'=>\app\Middlewares\VerifyCsrfToken::class,
-	        'session'=>\libs\Session\Middleware\StartSession::class
+			'auth_api'=>\app\Middlewares\AuthenticateApi::class,
+			'guest'=>\app\Middlewares\Guest::class,
+			'verify_email'=>\app\Middlewares\VerifyEmail::class,
+			'csrf'=>\app\Middlewares\VerifyCsrfToken::class,
+			'session'=>\libs\Session\Middleware\StartSession::class
 		]);
 	}
 
