d4d6c61479a507bdb4ca5b54ceeede07d7342865
dogiloki <wolftri2002@gmail.com>
2023-10-29 12:06:02 -0600

    refactor: less code in the index.php

--- public/index.php
+++ public/index.php
@@ -2,51 +2,14 @@
 
 chdir("../");
 
+// Llamado de autoload
 require_once "vendor/autoload.php";
+
+// Lamado de helpers
 require_once "libs/helpers.php";
 
-use libs\Config;
-
-function listDirectory($path){
-	if(!is_dir($path) || !file_exists($path) || !is_readable($path) || $path==""){
-		return [];
-	}
-	$json=[];
-	foreach(scandir($path) as $file){
-		if($file=="." || $file==".." || $file=="vendor" || $file==".git"){
-			continue;
-		}
-		$file_path=$path."/".$file;
-		if(is_dir($file_path)){
-			continue;
-		}
-		$json[slug($file_path)]=$file_path;
-	}
-	return $json;
-}
-
-//$env->set('APP_URL',str_replace("\\","/",(isset($_SERVER['HTTPS'])?"https":"http")."://".$_SERVER["HTTP_HOST"]).\dirname($_SERVER['PHP_SELF'])."/");
-// Verificar archivos publicos
-$uri=$_SERVER['REQUEST_URI'];
-if(file_exists(Config::filesystem("public.path").$uri) && !is_dir(Config::filesystem("public.path").$uri)){
-	$ext=explode(".",$uri)[1];
-	$mime=match($ext){
-		"css"=>"text/css",
-		"js"=>"text/javascript",
-		"png"=>"image/png",
-		"jpg"=>"image/jpg",
-		"jpeg"=>"image/jpeg",
-		"gif"=>"image/gif",
-		"svg"=>"image/svg+xml",
-		default=>"text/plain"
-	};
-	header("Content-Type: ".$mime);
-	echo file_get_contents(Config::filesystem("public.path").$uri);
-	exit;
-}
-unset($uri);
-
 // Zona horaria
+use libs\Config;
 date_default_timezone_set(env('TIMEZONE',Config::app('timezone')??date_default_timezone_get()));
 
 // Llamdo del Kernel
@@ -56,6 +19,5 @@ $kernel=new Kernel();
 // Llamado del enrutamiento
 use libs\Router\Route;
 Route::controller();
-
 
 ?>
