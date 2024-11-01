37603856bbc1618da46f7562d611dd1ddd27ae49
dogiloki <wolftri2002@gmail.com>
2024-11-01 15:52:24 -0600

    fix: replace logo.png

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use libs\Router\Route;
 
 Route::get('/',function(){
-    session()->put('message',[
-        'status'=>'success',
-        'data'=>'Hola'
-    ]);
     return view('home');
 })->name('home');
 
